03d77a340d46c73edbdf1e5decd6e0e0f746b42b
Aleksandr <alekcandr206@gmail.com>
2024-01-14 16:29:17 +0100

    init commit

---
+++ src/main/java/com/samedov/springbootexplore/SpringBootExploreApplication.java
@@ -0,0 +1,13 @@
+package com.samedov.springbootexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExploreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samedov/springbootexplore/SpringBootExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samedov.springbootexplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExploreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
