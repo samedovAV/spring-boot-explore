63a10d42dd4d2e0c9043d6fae993e836817f09f9
Aleksandr Samedov <alekcandr206@gmail.com>
2024-04-28 21:40:30 +0200

    update dependencies

--- src/main/java/com/samedov/springbootexplore/SpringBootExploreApplication.java
+++ src/main/java/com/samedov/springbootexplore/SpringBootExploreApplication.java
@@ -3,9 +3,11 @@ package com.samedov.springbootexplore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
+@EnableJpaRepositories
 public class SpringBootExploreApplication {
 
     public static void main(String[] args) {
